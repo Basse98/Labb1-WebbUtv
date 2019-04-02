477a71b22ce834294b81fea06e735298f4d5a377
Sebastian <sebastian.aronsson@iths.se>
2019-04-02 12:41:37 +0200

    Moved function out of request

--- App/js_code.js
+++ App/js_code.js
@@ -1,36 +1,65 @@
 
-const quizContainer = document.getElementById("quiz");
-const resultsContainer = document.getElementById("results");
-const submitButton = document.getElementById("submitButton");
-submitButton.addEventListener("click",showResults());
 
-function showResults(){
+
+var incorrectAnswers;
+var index = 1;
+
+function initQuestions(result){
+    questions = [];
+    var questions2 = [];
+    var correctAnswers = [];
+    incorrectAnswers = [];
+    currentIndex = 0;
+    result.forEach(function(product){
+        var para = document.createElement("p");
+        var element = document.getElementById("list");
+
+
+
+        var htmltest = "<div><p>"+"Question "+index+". "+product.question+"<p/>"
+        htmltest += "<button type='button'>True</button>";
+        htmltest += "<button type='button'>False</button>";
+        htmltest +="</div>"
+        para.innerHTML = htmltest;
+        element.appendChild(para);
+        index++;
+
+
+        questions.push(product.question);
+        questions2.push(product.question);
+        correctAnswers.push(product.correct_answer);
+        incorrectAnswers.push(product.incorrect_answers.toString());
+
+    });
+    /*
+    console.log("Hello from init function");
+    console.log(questions);
+    console.log(questions2);
+    console.log(correctAnswers);
+    console.log(incorrectAnswers);
+    */
 
 }
 
-function buildQuiz(){
-    const output =[];
+function start(){
 
 }
 
-var questions=[];
+
 var res;
-var test = new XMLHttpRequest;
-test.onreadystatechange = function(){
+var httpReq = new XMLHttpRequest;
+httpReq.onreadystatechange = function(){
     if(this.readyState === 4 ){
-        if(test.status === 200){
-            res = test.response;
-            res.results.forEach(function(product){
-                var para = document.createElement("p");
-                para.innerHTML = product.question;
-                var element = document.getElementById("list").appendChild(para);
-            });
+        if(httpReq.status === 200){
+            res = httpReq.response;
+            initQuestions(res.results);
+            start();
         }else{
-            console.log("HTTP-status är inte 200, utan " + test.status);
+            console.log("HTTP-status är inte 200, utan " + httpReq.status);
         }
     }
 
 };
-test.open("GET"," https://opentdb.com/api.php?amount=10&category=27&type=boolean");
-test.responseType="json";
-test.send();
+httpReq.open("GET"," https://opentdb.com/api.php?amount=10&category=27&type=boolean");
+httpReq.responseType="json";
+httpReq.send();
