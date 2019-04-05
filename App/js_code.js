19303e149d16a7a1e5f603190c18384abda3b13e
Sebastian <sebastian.aronsson@iths.se>
2019-04-05 10:59:04 +0200

    Remove unused function + add commments

--- App/js_code.js
+++ App/js_code.js
@@ -9,13 +9,16 @@ var numOfQuestions = 0;
 function initQuestions(result){
 
     correctAnswers = [];
+    //Loops through all the JSON results
     result.forEach(function(product){
         numOfQuestions++;
+
         allData.push(product);
         var para = document.createElement("div");
         var element = document.getElementById("list");
         correctAnswers.push(product.correct_answer);
 
+        //Creates HTML elements
         var htmlText = "<p>"+"Question "+index+". "+product.question+"</p>";
         htmlText += "<div class='buttonDiv'>";
         htmlText +="<button type='button' id='trueButton"+index+"' value='True'>True</button>";
@@ -26,10 +29,12 @@ function initQuestions(result){
         index++;
     });
 
+    //Saves values from buttoms and disables them when clicked
     userAnswers = [];
     var query = document.querySelector("main");
     query.addEventListener("click",function(e){
-        //console.log(e.target);
+
+        //Checking to see if the target is a button
         if(e.target.nodeName === "BUTTON"){
             if(e.target.id !== "submitButton"){
                 var parent = e.target.parentNode;
@@ -39,17 +44,10 @@ function initQuestions(result){
             }else{
                 e.target.disabled = true;
             }
-            /*
-            if(e.target.id !== "submitButton") {
-                userAnswers.push(e.target.value);
-                disableButtons(e.target.id);
-            }else{
-                disableButtons(e.target.id);
-            }
-            */
         }
     });
 
+    //Finishes the quiz via the submit button, prints out result
     var submitButton = document.getElementById("submitButton");
     submitButton.addEventListener("click", () => (
         userAnswers.forEach((answer, index)=> {
@@ -64,12 +62,7 @@ function initQuestions(result){
 
 }
 
-function disableButtons(id){
-    document.getElementById(id).disabled = true;
-    document.getElementById(id).nextSibling.disabled = true;
-}
-
-
+//Hämtar data från API
 var res;
 var httpReq = new XMLHttpRequest;
 httpReq.onreadystatechange = function(){
